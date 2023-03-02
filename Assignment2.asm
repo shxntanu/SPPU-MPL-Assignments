94b107c306222bbd5b70f3ae95097b66968d6efc
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-03-02 13:00:09 +0530

    Create Assignment2.asm

---
+++ Assignment2.asm
@@ -0,0 +1,56 @@
+section .data
+str: db "Enter a string: ", 0xA
+len: equ $-str
+opmsg: db "Length of string is: "
+len2: equ $-opmsg
+
+section .bss
+str1: resb 10
+leng: resb 1
+
+section .text
+global _start
+
+_start:
+
+mov rax,1
+mov rdi, 1
+mov rsi, str
+mov rdx, len
+syscall
+
+mov rax,0
+mov rdi, 0
+mov rsi, str1
+mov rdx, 20
+syscall
+
+dec al
+cmp al ,09h
+jbe L1
+
+; ADD 07H TO GET ASCII FROM HEX VALUE
+; IF CHAR IS BETWEEN A AND F
+add al, 07h
+
+; ADDING 30H TO GET ASCII FROM HEX VALUE
+; IF CHAR IS BETWEEN 0 AND 9
+L1:
+add al, 30h
+mov byte[leng] ,al
+
+mov rax,1
+mov rdi, 1
+mov rsi, opmsg
+mov rdx, len2
+syscall
+
+mov rax,1
+mov rdi, 1
+mov rsi, leng
+mov rdx, 1
+syscall
+
+mov rax, 60
+mov rdi, 00
+syscall

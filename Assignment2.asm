042c2aebaba67ac0bb4ab4605d39ac744b686a98
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-03-09 00:33:44 +0530

    Update Assignment2.asm

--- Assignment2.asm
+++ Assignment2.asm
@@ -1,59 +1,60 @@
 section .data
-str: db "Enter a string: ", 0xA
-len: equ $-str
-opmsg: db "Length of string is: "
-len2: equ $-opmsg
+str1: db "Enter a string: ",0xA
+len1 equ $-str1
+str2: db "The length of the string is: ",0xA
+len2 equ $-str2
+
 
 section .bss
-str1: resb 10
-leng: resb 2
+sent: resb 100
+ans: resb 1
 
 section .text
 global _start
-
 _start:
 
-                      ; "Enter a string: "
-mov rax,1
-mov rdi, 1
-mov rsi, str
-mov rdx, len
+mov rax,01          ;To print the statement "Enter a string: "
+mov rdi,01
+mov rsi,str1
+mov rdx,len1
 syscall
 
-                      ; Reading the string
-mov rax,0
-mov rdi, 0
-mov rsi, str1
-mov rdx, 20
+mov rax,00          ;to input the string
+mov rdi,00
+mov rsi,sent
+mov rdx,200
 syscall
 
-dec al                ; Decrement al to compensate for trailing null character
-mov  rsi , length
-cmp al ,09h
-jbe L1
+dec RAX             ;Decrement RAX to compensate for null character
+mov r8,ans          ;Store value of ans in r8
+mov cx, 16
+up:
+rol RAX,4
+mov bl,al
+and bl,0Fh
 
-; ADD 07H TO GET ASCII FROM HEX VALUE
-; IF CHAR IS BETWEEN A AND F
-add al, 07h
+cmp bl,09H
+JBE l1
+add bl,07H
+l1:
+add bl,30H
+mov byte[r8],bl
+inc r8
+dec cx
+JNZ up
 
-; ADDING 30H TO GET ASCII FROM HEX VALUE
-; IF CHAR IS BETWEEN 0 AND 9
-L1:
-add al, 30h
-mov byte[leng] ,al
-
-mov rax,1
-mov rdi, 1
-mov rsi, opmsg
-mov rdx, len2
+mov rax,01
+mov rdi,01
+mov rsi,str2
+mov rdx,len2
 syscall
 
-mov rax,1
-mov rdi, 1
-mov rsi, leng
-mov rdx, 1
+mov rax,01
+mov rdi,01
+mov rsi,ans
+mov rdx,16
 syscall
 
-mov rax, 60
-mov rdi, 00
+mov rax,60
+mov rdi,00
 syscall
